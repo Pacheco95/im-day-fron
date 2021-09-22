a5f0efcfcdc696ca15f11e8a866d56d1d9504394
Michael Pacheco <mdpgd95@gmail.com>
2021-09-22 01:31:17 -0300

    feat: add ScheduleService

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Table from './components/Table';
 import { UserDto } from './dto/UserDto';
+import scheduleService from './service/ScheduleService';
 
-const users: Readonly<UserDto[]> = Array(11)
-  .fill(0)
-  .map((_, i) => ({
-    id: i,
-    name: 'Michael',
-  }));
+const App: React.FC = () => {
+  const [scheduledUsers, setScheduledUsers] = useState<(UserDto | null)[]>([]);
 
-const App: React.FC = () => (
-  <>
-    <Header />
-    <main className="px-2 md:w-4/5 xl:w-3/4 mt-12 m-auto">
-      <Table
-        data={users.map(({ id: key, name: value }) => ({ key, value }))}
-        title="Usuários agendados"
-        subtitle={`Últimos ${users.length} dias`}
-      />
-    </main>
-  </>
-);
+  useEffect(() => {
+    scheduleService
+      .get<UserDto[]>('im-day')
+      .then(({ data }) => data)
+      .then((response) => setScheduledUsers(response));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className="px-2 md:w-4/5 xl:w-3/4 mt-12 m-auto">
+        <Table
+          data={scheduledUsers.map((user) => ({
+            key: user?.id || '-1',
+            value: user?.name || 'Ninguém',
+          }))}
+          title="IMs PP - Quem resolve?"
+          subtitle="Próximos dias"
+        />
+      </main>
+    </>
+  );
+};
 
 export default App;
---
+++ src/service/ScheduleService.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://im-day-api.herokuapp.com',
+});
